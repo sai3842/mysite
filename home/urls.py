aae13e9a41474db02f855070339c3f0c86f33084
sai3842 <saidatt4318@gmail.com>
2025-09-03 21:01:09 +0530

    urls.py

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns=[
     path('',views.homes,name='home'),
     path('h/',views.h),
-    path('profile',views.prfile_view,name='profile')
+    path('profile',views.prfile_view,name='profile'),
      path("createsu/", views.create_superuser, name="create_superuser"),
 ]
